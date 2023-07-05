2309ba30ccd130859cdb1e9ee6348c1e11ad7045
Võ Nguyễn Hoài Nam <vonguyenhoainam123@gmail.com>
2023-07-05 11:53:39 +0700

    update requirement for Netlify
    add .toml

---
+++ Funcrions/getJson.js
@@ -0,0 +1,9 @@
+exports.handler = function (event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(require('./models/model.json')),
+  });
+};
